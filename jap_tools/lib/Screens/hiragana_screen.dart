dd30eb1a94512d026fe1b50301feb8d2fef31a95
edewindt <edewindt27@outlook.com>
2023-02-18 00:33:16 -0600

    successfully loading data

--- jap_tools/lib/Screens/hiragana_screen.dart
+++ jap_tools/lib/Screens/hiragana_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:jap_tools/Widgets/character_widget.dart';
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
 
 class HiraganaScreen extends StatelessWidget {
   const HiraganaScreen({super.key});
   static const title = "Hiragana/ひらがな";
   @override
   Widget build(BuildContext context) {
+    readJson();
     return Scaffold(
       body: GridView(
         children: [CharacterWidget(title: "さ")],
@@ -21,4 +25,12 @@ class HiraganaScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+Future<void> readJson() async {
+  final String response =
+      await rootBundle.loadString('Data/hiragana_characters.json');
+  final data = await json.decode(response);
+  print(response);
+// ...
 }
